c1b622aa10a2e7100bedcdd4815126d8ebe127d8
SRparadox <157661887+SRparadox@users.noreply.github.com>
2024-02-09 21:02:37 -0800

    Fix

---
+++ src/Main.js
@@ -0,0 +1,22 @@
+// Name:Shazer Rizzo
+// Date: 2/8/2024
+
+'use strict'
+
+const config = {
+    parent: 'phaser-game',  // for info text
+    type: Phaser.WEBGL,     // for tinting
+    width: 400,
+    height: 300,
+    pixelArt: true,
+    zoom: 2,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    },
+    scene: [ Load, Play ]
+}
+
+const game = new Phaser.Game(config)
